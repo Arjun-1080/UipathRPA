dd0bcd33c921d76fded6149c2818b78c2fc155cb
Arjun-1080 <mallikarjunsawakar06@gmail.com>
2024-11-24 01:41:49 +0530

    Done with events and Blog page

--- client/src/pages/Events.tsx
+++ client/src/pages/Events.tsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 function Events() {
   return (
-    <div></div>
+    <div className='h-[calc(100vh-70px)]  w-full flex flex-col justify-center items-center'>
+      <h1 className='font-source text-[60px]'>Stay Tuned! 🚀</h1>
+      <h1 className='font-source text-[20px]'>We're currently planning some amazing events for you. Keep an eye on this space for updates!"</h1>
+    </div>
   )
 }
 
